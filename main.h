4b889d22f76b8f0d2357afe32672218915b6182c
Dembele <Dembele@bk.ru>
2015-09-26 09:42:32 +0300

    upd

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <iostream>
+#include <sys/ioctl.h>
+#include <cstdio>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <termios.h>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include "defines.h"
+#include "getch.h"
+#endif // MAIN_H
